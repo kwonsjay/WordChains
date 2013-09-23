e1f709db10084ead77401c2bc820b3c33ea921d1
SoonJae Kwon <kwonsjay@gmail.com>
2013-09-22 18:55:56 -0700

    implement methods to find and build word path

--- word_chain/word_chain.rb
+++ word_chain/word_chain.rb
@@ -1,6 +1,6 @@
 class WordChains
   #Set accessors
-  attr_accessor :dictionary, :start_word, :end_word, :flag
+  attr_accessor :dictionary, :start_word, :end_word, :visited_words, :flag
   
   #Initialize WordChains with starting and ending words, as well as dictionary
   #Limit dictionary to include only cleaned words of the same length as inputs
@@ -12,6 +12,7 @@ class WordChains
     end.select { |word| word && word.length == start_word.length }
     @visited_words = {}
     @skip_indices = []
+    @flag = false
   end
   
   #Reduce the dictionary further if start and end word both contain a character
@@ -29,6 +30,7 @@ class WordChains
   end
   
   #Given a word, find all adjacent unique words that are in the dictionary
+  #Record visited
   def adjacent_words(word)
     new_words_array = []
     word.split("").each_with_index do |char, index|
@@ -45,8 +47,34 @@ class WordChains
     new_words_array
   end
   
+  #Set up the search for the end word
   def find_path
-    
+    visited_words[@start_word] = nil
+    current_words = [@start_word]
+    until @flag do
+      raise ArgumentException, "No Path Found!" if current_words.empty?
+      current_word = current_words.shift
+      new_words = adjacent_words(current_word)
+      if new_words.include?(@end_word)
+        @flag = true
+        puts "Path Found!"
+        return build_path
+      end
+      new_words.each do |new|
+        current_words << new
+      end
+    end
   end
   
+  #If end word found, build the path up to the start word
+  def build_path
+    path = [@end_word]
+    check_word = @visited_words[@end_word]
+    until check_word.nil?
+      path << check_word
+      check_word = @visited_words[check_word]
+    end
+    path.reverse
+  end
+
 end
